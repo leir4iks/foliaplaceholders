9821326f9fcc0a5c16a1568ba42140890c1e1478
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-05-12 13:36:26 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.4-R0.1") // попробуйте без SNAPSHOT
-
+    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
     compileOnly("dev.folia:folia-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
