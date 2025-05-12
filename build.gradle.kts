e0d94de8dc4bc3aadf8595d58ff89f7e61c49528
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-05-12 13:02:30 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "com.example"
+version = "1.0.0"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    compileOnly("dev.folia:folia-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+}
+
+tasks {
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveBaseName.set("foliaplaceholders")
+        archiveVersion.set("1.0.0")
+        archiveClassifier.set("")
+        archiveExtension.set("jar")
+        // relocate("me.clip.placeholderapi", "com.foliaplaceholders.shaded.placeholderapi")
+        mergeServiceFiles()
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
